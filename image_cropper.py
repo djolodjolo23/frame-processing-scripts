01a467611ffe31793c78d45126fe63df713576f6
Djordje Dimitrov <dd222im@student.lnu.se>
2024-04-05 11:05:17 +0200

    Changed frame saving format to png. Seems like png preserves quality better than jpg, so we will use png.

--- image_cropper.py
+++ image_cropper.py
@@ -1,12 +1,20 @@
 from PIL import Image, ImageDraw
 import xml.etree.ElementTree as ET
 import os
+import argparse
 
-predefined_square_size = 400  #testing with 400x400
+parser = argparse.ArgumentParser(description='Process images and annotations for object detection.')
+parser.add_argument('--target_image_size', type=int, default=244, help='Target image size for the square crop.')
+parser.add_argument('--video_name', type=str, required=True, help='Name of the video for processing.')
+parser.add_argument('--initial_xml', type=str, required=True, help='Path to the initial XML annotations file.')
+
+args = parser.parse_args()
+
+target_image_size = args.target_image_size
+video_name = args.video_name
+initial_xml = args.initial_xml
+
 additional_padding = 10
-video_name = 'GX011088'
-folder_path = 'frames'
-initial_xml = 'annotations.xml'
 
 
 cropped_folder_path = f'cropped/{video_name}'
@@ -20,7 +28,7 @@ for track in root.findall('.//track'):
         for box in track.findall('box'):
             frame_num = box.attrib['frame']
 
-            image_path = os.path.join(folder_path, f'frames_{video_name}/frame_{frame_num}.jpg')
+            image_path = os.path.join(f'frames/frames_{video_name}/frame_{frame_num}.png')
             if not os.path.exists(image_path):
                 continue
 
@@ -32,12 +40,12 @@ for track in root.findall('.//track'):
 
             initial_square_size = max(bbox_width, bbox_height)
 
-            predefined_square_size_temp = predefined_square_size
-            if initial_square_size > predefined_square_size:
-                predefined_square_size = round(initial_square_size) + additional_padding # adding predefined padding pixels to the square size to make sure the object is not cropped out
+            predefined_square_size_temp = target_image_size
+            if initial_square_size > target_image_size: # if the bbox is larger than the predefined square size
+                target_image_size = round(initial_square_size) + additional_padding # adding predefined padding pixels to the square size to make sure the object is not cropped out
 
-            padding_height = (predefined_square_size - bbox_height) / 2
-            padding_width = (predefined_square_size - bbox_width) / 2
+            padding_height = (target_image_size - bbox_height) / 2
+            padding_width = (target_image_size - bbox_width) / 2
 
             # the new square crop area values
             crop_left = xtl - padding_width
@@ -46,16 +54,16 @@ for track in root.findall('.//track'):
             crop_lower = ybr + padding_height
 
             cropped_area = original_image.crop((int(crop_left), int(crop_upper), int(crop_right), int(crop_lower)))
-            new_image = Image.new('RGB', (predefined_square_size, predefined_square_size), (255, 255, 255))
+            new_image = Image.new('RGB', (target_image_size, target_image_size), (255, 255, 255))
 
             # pasting the cropped image to the new image
-            paste_x = (predefined_square_size - cropped_area.width) // 2
-            paste_y = (predefined_square_size - cropped_area.height) // 2
+            paste_x = (target_image_size - cropped_area.width) // 2
+            paste_y = (target_image_size - cropped_area.height) // 2
             new_image.paste(cropped_area, (paste_x, paste_y))
 
-            if predefined_square_size_temp != predefined_square_size:
+            if predefined_square_size_temp != target_image_size:
                 new_image = new_image.resize((predefined_square_size_temp, predefined_square_size_temp))
-                resize_ratio = predefined_square_size_temp / predefined_square_size
+                resize_ratio = predefined_square_size_temp / target_image_size
                 new_tlx = padding_width * resize_ratio
                 new_tly = padding_height * resize_ratio
                 new_brx = (padding_width * resize_ratio) + (bbox_width * resize_ratio)
@@ -66,16 +74,17 @@ for track in root.findall('.//track'):
                 new_brx = padding_width + bbox_width
                 new_bry = padding_height + bbox_height
 
+            # for new annotation.xml file
             box.set('xtl', str(new_tlx))
             box.set('ytl', str(new_tly))
             box.set('xbr', str(new_brx))
             box.set('ybr', str(new_bry))
 
             draw = ImageDraw.Draw(new_image)
-            draw.rectangle([(new_tlx, new_tly), (new_brx, new_bry)], outline='red', width=3)
+            draw.rectangle([new_tlx, new_tly, new_brx, new_bry], outline='red', width=2) #test
 
-            new_image.save(f'cropped/{video_name}/cropped_frame{frame_num}.jpg')
-            predefined_square_size = predefined_square_size_temp
+            new_image.save(f'cropped/{video_name}/cropped_frame{frame_num}.png')
+            target_image_size = predefined_square_size_temp
 
 new_xml_path = f'annotations_updated_{video_name}.xml'
 tree.write(new_xml_path)
