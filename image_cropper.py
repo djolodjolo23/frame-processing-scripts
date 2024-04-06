e494961247240091ffb48cca759b48f88808031a
Djordje Dimitrov <dd222im@student.lnu.se>
2024-04-06 08:43:26 +0200

    Updated file paths. Added the pascal_voc.py to create the pascal voc xml file for each of the cropped images.

--- image_cropper.py
+++ image_cropper.py
@@ -2,22 +2,25 @@ from PIL import Image, ImageDraw
 import xml.etree.ElementTree as ET
 import os
 import argparse
+from pascal_voc import write_pascal_voc
 
-parser = argparse.ArgumentParser(description='Process images and annotations_CVAT for object detection.')
-parser.add_argument('target_image_size', type=int, help='Target image size for the square crop.')
-parser.add_argument('video_name', type=str, help='Name of the video for processing.')
-parser.add_argument('annotation_path', type=str, help='Path to the initial XML annotations_CVAT file.')
+#parser = argparse.ArgumentParser(description='Process images and annotations_CVAT for object detection.')
+#parser.add_argument('target_image_size', type=int, help='Target image size for the square crop.')
+#parser.add_argument('video_name', type=str, help='Name of the video for processing.')
+#parser.add_argument('annotation_path', type=str, help='Path to the initial XML annotations_CVAT file.')
 
-args = parser.parse_args()
+#args = parser.parse_args()
 
-target_image_size = args.target_image_size
-video_name = args.video_name
-annotation_path = args.annotation_path
+target_image_size = 244
+video_name = 'GX0110088'
+annotation_path = 'annotations_CVAT/annotations_88.xml'
 
 additional_padding = 10
 
-cropped_folder_path = f'cropped/{video_name}'
+cropped_folder_path = f'cropped/{video_name}/frames'
+cropped_annotations_folder_path = f'cropped/{video_name}/annotations'
 os.makedirs(cropped_folder_path, exist_ok=True)
+os.makedirs(cropped_annotations_folder_path, exist_ok=True)
 
 print(f"Processing annotations_CVAT from: {annotation_path}")
 
@@ -66,29 +69,31 @@ for track in root.findall('.//track'):
             if predefined_square_size_temp != target_image_size:
                 new_image = new_image.resize((predefined_square_size_temp, predefined_square_size_temp))
                 resize_ratio = predefined_square_size_temp / target_image_size
-                new_tlx = padding_width * resize_ratio
-                new_tly = padding_height * resize_ratio
-                new_brx = (padding_width * resize_ratio) + (bbox_width * resize_ratio)
-                new_bry = (padding_height * resize_ratio) + (bbox_height * resize_ratio)
+                new_xtl = padding_width * resize_ratio
+                new_ytl = padding_height * resize_ratio
+                new_xbr = (padding_width * resize_ratio) + (bbox_width * resize_ratio)
+                new_ybr = (padding_height * resize_ratio) + (bbox_height * resize_ratio)
             else:
-                new_tlx = padding_width
-                new_tly = padding_height
-                new_brx = padding_width + bbox_width
-                new_bry = padding_height + bbox_height
+                new_xtl = padding_width
+                new_ytl = padding_height
+                new_xbr = padding_width + bbox_width
+                new_ybr = padding_height + bbox_height
 
             # for new annotation.xml file
-            box.set('xtl', str(new_tlx))
-            box.set('ytl', str(new_tly))
-            box.set('xbr', str(new_brx))
-            box.set('ybr', str(new_bry))
+            box.set('xtl', str(new_xtl))
+            box.set('ytl', str(new_ytl))
+            box.set('xbr', str(new_xbr))
+            box.set('ybr', str(new_ybr))
 
             draw = ImageDraw.Draw(new_image)
+
+            write_pascal_voc(f'{cropped_annotations_folder_path}/frame_{frame_num}.xml', f'frame_{frame_num}.png', predefined_square_size_temp, new_xtl, new_ytl, new_xbr, new_ybr)
 
             # TODO: if you want to check if the bounding boxes within the cropped images are correct, uncomment the
             #  following line
             #draw.rectangle([new_tlx, new_tly, new_brx, new_bry], outline='red', width=2) #test
 
-            new_image.save(f'cropped/{video_name}/frame_{frame_num}.png')
+            new_image.save(f'{cropped_folder_path}/frame_{frame_num}.png')
             target_image_size = predefined_square_size_temp
 
 new_xml_path = os.path.join(cropped_folder_path, 'annotations.xml')
---
+++ pascal_voc.py
@@ -0,0 +1,46 @@
+
+
+def write_pascal_voc(file_path, filename, square_edge, xtl, ytl, xbr, ybr):
+    with open(file_path, 'w') as file:
+        xml_content = f'''<annotation>
+          <folder>frame</folder>
+          <filename>{filename}</filename>
+          <source>
+            <database>Unknown</database>
+            <annotation>Unknown</annotation>
+            <image>Unknown</image>
+          </source>
+          <size>
+            <width>{square_edge}</width>
+            <height>{square_edge}</height>
+            <depth></depth>
+          </size>
+          <segmented>0</segmented>
+          <object>
+            <name>queen</name>
+            <truncated>0</truncated>
+            <occluded>0</occluded>
+            <difficult>0</difficult>
+            <bndbox>
+              <xmin>{xtl}</xmin>
+              <ymin>{ytl}</ymin>
+              <xmax>{xbr}</xmax>
+              <ymax>{ybr}</ymax>
+            </bndbox>
+            <attributes>
+              <attribute>
+                <name>rotation</name>
+                <value>0.0</value>
+              </attribute>
+              <attribute>
+                <name>track_id</name>
+                <value>0</value>
+              </attribute>
+              <attribute>
+                <name>keyframe</name>
+                <value>True</value>
+              </attribute>
+            </attributes>
+          </object>
+        </annotation>'''
+        file.write(xml_content)
