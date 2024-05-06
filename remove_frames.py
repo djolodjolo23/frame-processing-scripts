241848fbb1b7fccc386705d4636478353c05b167
Djordje Dimitrov <dd222im@student.lnu.se>
2024-05-06 14:50:30 +0200

    Just an update to remove all frames at once.

--- remove_frames.py
+++ remove_frames.py
@@ -13,7 +13,8 @@ def process_files(folder_path):
         print(f"Keeping: {files[i]}")
         
         # Skip the next four files and delete them
-        for _ in range(4):
+
+        for _ in range(1):
             i += 1
             if i < len(files):
                 file_to_delete = os.path.join(folder_path, files[i])
@@ -25,4 +26,15 @@ def process_files(folder_path):
         # Move to the next file to keep
         i += 1
 
-process_files('compressed/micro_all_frames_training/images')
+process_files('frames/micro1')
+process_files('frames/micro2')
+process_files('frames/micro3')
+process_files('frames/micro4')
+process_files('frames/micro5')
+process_files('frames/micro6')
+process_files('frames/micro7')
+process_files('frames/micro8')
+process_files('frames/micro9')
+process_files('frames/micro10')
+
+
